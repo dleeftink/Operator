43d76584bcab19c3c0ccebedb31ed8c010bdaa40
dleeftink <dennisleeftink@gmail.com>
2025-02-25 03:08:27 +1000

    Update Builder.js

--- core/Builder.js
+++ core/Builder.js
@@ -1,86 +1,111 @@
 class Base {
-  
-  #debug = false;
-  constructor(input) {
-    if (input === Base) {
-      // Aim to create a new branch that benefits from cached results
-      return new (class Branch extends Base {})(this);
-    } else if (input === Array) {
-      return Array.from(this);
-    } else if (input === Set) {
-      if (this.constructor.Set) return new Set(this.constructor.Set);
-      else return consume(this, input);
-    } else if (input === Map) {
-      if (this.constructor.Map) return new Set(this.constructor.Map);
-      else return consume(this, input);
-    } else if (input instanceof Base) {
-      this.constructor.data = input; // import existing branch
-    } else if (input) {
-      this.data = input;
+
+    #debug = false;
+    constructor(input,debug) {
+      if (input === Base) {
+        // Create a new branch that benefits from cached results
+        return new (class Branch extends Base {})(this);
+      } else if (input === Array) {
+        if (this.constructor.Array) return new Set(this.constructor.Array);
+        return (this.constructor.Array = Array.from(this));
+      } else if (input === Set) {
+        if (this.constructor.Set) return new Set(this.constructor.Set);
+        return consume(this, input); 
+      } else if (input === Map) {
+        if (this.constructor.Map) return new Set(this.constructor.Map);
+        return consume(this, input); 
+      } else if (input instanceof Base) {
+        if(debug) console.log("Hoisting cache from",input, input.constructor.cache)
+        this.constructor.data = input/*.constructor.cache*/; // import existing branch
+      } else if (input) {
+        this.data = input;
+      }
+      if(debug) this.#debug = debug
     }
-  }
 
-  [Symbol.iterator]() {
-    let source = (this.data ?? this.constructor.data)[Symbol.iterator]();
-    return source;
-  }
+    static extend(next = {}, debug = false) {
 
-  static extend(def = {}, debug = false) {
-    
-    // Setup reference chain
-    let prev = this instanceof Function ? this : this.constructor;
-    let data = this.data ?? this.constructor.data;
-    let root = Object.getPrototypeOf(prev).prototype ? prev.root : this;
-    debug = prev.debug ?? debug;
+      // Setup reference chain 
+      let prev = this instanceof Function ? this : this.constructor;
+      let data = this.data ?? this.constructor.data;
+      let root = Object.getPrototypeOf(prev).prototype ? prev.root : this;
+          debug = prev.debug ?? debug;
 
-    let {
-      name = "Layer",
-      methods,
-      statics,
-      evaluate,
-      transform,
-      predicate,
-      comparator,
-      cache: Cache = Array,
-    } = def;
+      let {
+        name = "Layer",
+        eager,
+        methods,
+        useCache,
+        statics,
+        evaluate,
+        transform,
+        predicate,
+        comparator,
+        cacheEnabled,
+        cache: Cache = Array
+      } = next;
 
-    // Initiate new cache layer if class extensions don't match
-    let init = name !== prev.name;
-    let cache = init ? new Cache() : prev.cache;
+      // Initiate new cache layer if class names don't match
+      
+      let init = name !== prev.name; 
+      let cache = init ? new Cache : prev.cache; cache.enabled = true;
+      
+      // First layer can't have cache currently, but likely won't need it
+      // Only cache the last layer or if secondary layers 'useCache' flag 
 
-    // Name is stored with class
-    let Layer = {
-      [name]: class extends prev {
-        static root = root;
-        static data = data;
-        static prev = prev;
-        static cache = cache;
-        static debug = debug;
+      if(init && prev.cache) prev.cache.enabled = (prev.useCache ?? false)
 
-        [Symbol.iterator]() {
-          // Run main iterator
-          const iterator = super[Symbol.iterator]();
-          return evaluate({
-            init,
-            name,
-            iterator: iterator,
-            comparator,
-            transform,
-            predicate,
-            cache,
-            debug,
-          });
+      // Name is dynamically assigned to class
+      // Remember : While setup code is run in order
+      // prototypes are ran outside in (while iterator inside out...)
+      
+      let Layer = ({
+        [name]: class extends prev {
+          static root = root;
+          static data = data;
+          static prev = prev;
+          static cache = cache;
+          static debug = debug;
+          static useCache = useCache;
+
+          [Symbol.iterator]() {
+
+            // Use the layer cache
+            if(cache.size || cache.length) {
+              if(debug) console.log("Layer cache used at",name,(predicate ?? transform ?? comparator).toString())
+              return cache[Symbol.iterator]()
+            }
+            
+            // Run custom iterator
+            const iterator = super[Symbol.iterator]();
+            return evaluate({
+              init,
+              name,
+              cache,
+              debug,
+              transform,
+              predicate,
+              comparator,
+              cacheEnabled,
+              iterator: iterator,
+            });
+          }
         }
-      },
-    }[name];
+      })[name];
 
-    if (methods) Object.assign(Layer.prototype, methods);
-    if (statics) Object.assign(Layer, statics);
+      if (methods) Object.assign(Layer.prototype, methods);
+      if (statics) Object.assign(Layer, statics);
 
-    return Layer;
+      return Layer;
+    }
+
+    extend(def) {
+      return this.constructor.extend.call(this, def, this.#debug);
+    }
+
+    [Symbol.iterator]() {
+      let source = (this.data ?? this.constructor.data)[Symbol.iterator]();
+      return source;
+    }
+
   }
-
-  extend(def) {
-    return this.constructor.extend.call(this, def, this.#debug);
-  }
-}
