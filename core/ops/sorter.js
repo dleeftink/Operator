f6227cfdde62ba88f48d53e51761452476e108d5
dleeftink <dennisleeftink@gmail.com>
2025-02-25 03:19:56 +1000

    Update sorter.js

--- core/ops/sorter.js
+++ core/ops/sorter.js
@@ -1,47 +1,39 @@
 function sorter(config) {
+  
   let { name, cache, iterator, comparator, debug, init } = config;
+  let { enabled } = { enabled: true }; // Always on;
 
-  let index = 0;
-  let isCaching = true;
-  let isSorted = false; // Track whether the cache is sorted
+  if (debug) console.log(name, "uses cache", enabled);
+
+  if (!cache.sorted) {
+    while (true) {
+      const { value, done } = iterator.next();
+      if (done) {
+        iterator = cache[Symbol.iterator]();
+        cache.sort(comparator);
+        cache.sorted = true;
+        break;
+      }
+      cache.push(value);
+    }
+  } else {
+    incrementalSort(cache,comparator)
+  }
 
   return {
     next: () => {
-      // Caching Phase: Add elements to Array cache
-      while (isCaching) {
-        const { value, done } = iterator.next();
-        if (done) {
-          isCaching = false;
-          break;
-        }
-        if (init) {
-          cache.push(value);
-        } else {
-          break; // Exit caching phase early if not initializing
-        }
-      }
+      let { value, done } = iterator.next();
 
-      // Initial Full Sort
-      if (!isSorted && cache.length > 0) {
-        cache.sort(comparator);
-        isSorted = true; // Mark as sorted after the first full sort
-      }
-
-      // Incremental Sort (for subsequent calls with a new comparator)
-      if (index === 0 && !init && isSorted) {
-        incrementalSort(cache, comparator);
-      }
-
-      // Yielding Phase: Return elements from the sorted cache
-      if (index < cache.length) {
-        return { value: cache[index++], done: false };
-      } else {
+      if (done) {
         if (debug) cacheLogger(name, init, comparator, cache);
-        return { value: undefined, done: true };
+        return { value, done };
       }
-    },
+
+      return { value, done };
+    }
   };
 }
+
 
 function incrementalSort(array, comparator) {
   let changed = true;
